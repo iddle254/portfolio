377f94ee4347072bf12d0409ab48f6cb10f4c2e7
Charles <charlesiddle99@gmail.com>
2020-03-16 12:28:04 +0300

    Updates

---
+++ precache-manifest.359abbe597777d0e0164a981b2fa9c23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e94f662c22b756352636cc27e545fd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6a7660dab9cf45388fcd",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "08cd1ac7ef520700efd5",
+    "url": "/portfolio/static/js/2.eabf0129.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.eabf0129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a7660dab9cf45388fcd",
+    "url": "/portfolio/static/js/main.6f1b8217.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
