71b00f21b42a4ba6e3cb22ddb945b281865be74e
Charles <charlesiddle99@gmail.com>
2020-03-22 00:34:59 +0300

    Updates

---
+++ precache-manifest.0dd48ecc9ff536bae4b72e7881e8304c.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c032be63f3f62d7dcbf0bfb0a092d0d4",
+    "url": "/M.campaign/index.html"
+  },
+  {
+    "revision": "1afe49017134f3447343",
+    "url": "/M.campaign/static/css/main.279aa1f8.chunk.css"
+  },
+  {
+    "revision": "5a6da302add9c14910d2",
+    "url": "/M.campaign/static/js/2.85b0540c.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/M.campaign/static/js/2.85b0540c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1afe49017134f3447343",
+    "url": "/M.campaign/static/js/main.a0965bf2.chunk.js"
+  },
+  {
+    "revision": "f62d31e69035dad62e90",
+    "url": "/M.campaign/static/js/runtime-main.e93f94d0.js"
+  },
+  {
+    "revision": "48461ea4e797c9774dabb4a0440d2f56",
+    "url": "/M.campaign/static/media/fa-brands-400.48461ea4.woff2"
+  },
+  {
+    "revision": "7b464e274bc331f9a765d765359635a5",
+    "url": "/M.campaign/static/media/fa-brands-400.7b464e27.woff"
+  },
+  {
+    "revision": "947b9537bc0fecc8130d48eb753495a1",
+    "url": "/M.campaign/static/media/fa-brands-400.947b9537.ttf"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "/M.campaign/static/media/fa-brands-400.9b6c8da3.eot"
+  },
+  {
+    "revision": "b5472631dbace30d549357ec325b9c62",
+    "url": "/M.campaign/static/media/fa-brands-400.b5472631.svg"
+  },
+  {
+    "revision": "381af09a1366b6c2ae65eac5dd6f0588",
+    "url": "/M.campaign/static/media/fa-regular-400.381af09a.woff"
+  },
+  {
+    "revision": "73fe7f1effbf382f499831a0a9f18626",
+    "url": "/M.campaign/static/media/fa-regular-400.73fe7f1e.ttf"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "/M.campaign/static/media/fa-regular-400.7422060c.eot"
+  },
+  {
+    "revision": "949a2b066ec37f5a384712fc7beaf2f1",
+    "url": "/M.campaign/static/media/fa-regular-400.949a2b06.woff2"
+  },
+  {
+    "revision": "b5a61b229c9c92a6ac21f5b0e3c6e9f1",
+    "url": "/M.campaign/static/media/fa-regular-400.b5a61b22.svg"
+  },
+  {
+    "revision": "0079a0ab6bec4da7d6e16f2a2e87cd71",
+    "url": "/M.campaign/static/media/fa-solid-900.0079a0ab.ttf"
+  },
+  {
+    "revision": "14a08198ec7d1eb96d515362293fed36",
+    "url": "/M.campaign/static/media/fa-solid-900.14a08198.woff2"
+  },
+  {
+    "revision": "38508b2e7fac045869a86a15936433f7",
+    "url": "/M.campaign/static/media/fa-solid-900.38508b2e.svg"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "/M.campaign/static/media/fa-solid-900.70e65a7d.eot"
+  },
+  {
+    "revision": "815694de1120d6c1e9d1f0895ee81056",
+    "url": "/M.campaign/static/media/fa-solid-900.815694de.woff"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/M.campaign/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/M.campaign/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/M.campaign/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/M.campaign/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/M.campaign/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "18b799a071dcd1311c5a039f6a867d38",
+    "url": "/M.campaign/static/media/profile-cover.18b799a0.jpg"
+  },
+  {
+    "revision": "a3cb348a3c9dd76435f243676478ddbc",
+    "url": "/M.campaign/static/media/react.a3cb348a.jpg"
+  }
+]);
